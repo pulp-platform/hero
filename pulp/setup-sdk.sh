1f6a197d8570dd3feeaa2d2197c9bd26b1e76a9d
Koen Wolters <kwolters@student.ethz.ch>
2019-09-13 12:50:31 +0200

    restructure linker configuration

--- pulp/setup-sdk.sh
+++ pulp/setup-sdk.sh
@@ -53,5 +53,5 @@ make env
 cd ${THIS_DIR}
 source ${THIS_DIR}/sdk/sourceme.sh
 mkdir -p ${PULP_SDK_HOME}/install/hero/${pulp_chip}
-${RISCV}/bin/riscv32-unknown-elf-gcc -Wextra -Wall -Wno-unused-parameter -Wno-unused-variable -Wno-unused-function -Wundef -fdata-sections -ffunction-sections -I${PULP_SDK_INSTALL}/include/io -I${PULP_SDK_INSTALL}/include -march=rv32imcxpulpv2 -D__riscv__ -include refs/${pulp_chip}/cl_config.h -c refs/${pulp_chip}/rt_conf.c -o ${PULP_SDK_HOME}/install/hero/${pulp_chip}/rt_conf.o
+${RISCV}/bin/riscv32-unknown-elf-gcc -Wextra -Wall -Wno-unused-parameter -Wno-unused-variable -Wno-unused-function -Wundef -fdata-sections -ffunction-sections -I${PULP_SDK_INSTALL}/include/io -I${PULP_SDK_INSTALL}/include -march=rv32imcxpulpv2 -D__riscv__ -include refs/${pulp_chip}/cl_config.h -c refs/rt_conf.c -o ${PULP_SDK_HOME}/install/hero/${pulp_chip}/rt_conf.o
 cp -r refs/* ${PULP_SDK_HOME}/install/hero/
