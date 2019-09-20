536ae17fc07fcd965dde00a2780a4ce18bb70f4a
Andreas Kurth <akurth@iis.ee.ethz.ch>
2019-09-20 09:09:15 +0200

    :art: hardware/axi_rab/axi4_ar_sender: Reformat to current style guide

--- hardware/deps/axi_rab/rtl/axi4_ar_sender.sv
+++ hardware/deps/axi_rab/rtl/axi4_ar_sender.sv
@@ -8,53 +8,54 @@
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the
 // specific language governing permissions and limitations under the License.
 
-module axi4_ar_sender
-  #(
-    parameter AXI_ADDR_WIDTH = 40,
-    parameter AXI_ID_WIDTH   = 4,
-    parameter AXI_USER_WIDTH = 4,
-    parameter ENABLE_L2TLB   = 0
-  )
-  (
-    input  logic                      axi4_aclk,
-    input  logic                      axi4_arstn,
+module axi4_ar_sender #(
+  parameter AXI_ADDR_WIDTH = 40,
+  parameter AXI_ID_WIDTH   = 4,
+  parameter AXI_USER_WIDTH = 4,
+  parameter ENABLE_L2TLB   = 0,
+  localparam type addr_t = logic [AXI_ADDR_WIDTH-1:0],
+  localparam type id_t = logic [AXI_ID_WIDTH-1:0],
+  localparam type user_t = logic [AXI_USER_WIDTH-1:0]
+) (
+  input  logic              axi4_aclk,
+  input  logic              axi4_arstn,
 
-    output logic                      l1_done_o,
-    input  logic                      l1_accept_i,
-    input  logic                      l1_drop_i,
-    input  logic                      l1_save_i,
+  output logic              l1_done_o,
+  input  logic              l1_accept_i,
+  input  logic              l1_drop_i,
+  input  logic              l1_save_i,
 
-    output logic                      l2_done_o,
-    input  logic                      l2_accept_i,
-    input  logic                      l2_drop_i,
-    output logic                      l2_sending_o,
+  output logic              l2_done_o,
+  input  logic              l2_accept_i,
+  input  logic              l2_drop_i,
+  output logic              l2_sending_o,
 
-    input  logic [AXI_ADDR_WIDTH-1:0] l1_araddr_i,
-    input  logic [AXI_ADDR_WIDTH-1:0] l2_araddr_i,
+  input  addr_t             l1_araddr_i,
+  input  addr_t             l2_araddr_i,
 
-    input  logic   [AXI_ID_WIDTH-1:0] s_axi4_arid,
-    input  logic                      s_axi4_arvalid,
-    output logic                      s_axi4_arready,
-    input  logic                [7:0] s_axi4_arlen,
-    input  logic                [2:0] s_axi4_arsize,
-    input  logic                [1:0] s_axi4_arburst,
-    input  logic                      s_axi4_arlock,
-    input  logic                [2:0] s_axi4_arprot,
-    input  logic                [3:0] s_axi4_arcache,
-    input  logic [AXI_USER_WIDTH-1:0] s_axi4_aruser,
+  input  id_t               s_axi4_arid,
+  input  logic              s_axi4_arvalid,
+  output logic              s_axi4_arready,
+  input  axi_pkg::len_t     s_axi4_arlen,
+  input  axi_pkg::size_t    s_axi4_arsize,
+  input  axi_pkg::burst_t   s_axi4_arburst,
+  input  logic              s_axi4_arlock,
+  input  axi_pkg::prot_t    s_axi4_arprot,
+  input  axi_pkg::cache_t   s_axi4_arcache,
+  input  user_t             s_axi4_aruser,
 
-    output logic   [AXI_ID_WIDTH-1:0] m_axi4_arid,
-    output logic [AXI_ADDR_WIDTH-1:0] m_axi4_araddr,
-    output logic                      m_axi4_arvalid,
-    input  logic                      m_axi4_arready,
-    output logic                [7:0] m_axi4_arlen,
-    output logic                [2:0] m_axi4_arsize,
-    output logic                [1:0] m_axi4_arburst,
-    output logic                      m_axi4_arlock,
-    output logic                [2:0] m_axi4_arprot,
-    output logic                [3:0] m_axi4_arcache,
-    output logic [AXI_USER_WIDTH-1:0] m_axi4_aruser
-  );
+  output id_t               m_axi4_arid,
+  output addr_t             m_axi4_araddr,
+  output logic              m_axi4_arvalid,
+  input  logic              m_axi4_arready,
+  output axi_pkg::len_t     m_axi4_arlen,
+  output axi_pkg::size_t    m_axi4_arsize,
+  output axi_pkg::burst_t   m_axi4_arburst,
+  output logic              m_axi4_arlock,
+  output axi_pkg::prot_t    m_axi4_arprot,
+  output axi_pkg::cache_t   m_axi4_arcache,
+  output user_t             m_axi4_aruser
+);
 
   logic l1_save;
 
@@ -72,18 +73,17 @@ module axi4_ar_sender
   assign s_axi4_arready = (m_axi4_arvalid & m_axi4_arready & ~l2_sending_o) |
                           (s_axi4_arvalid & (l1_drop_i | l1_save));
 
-generate
-  if (ENABLE_L2TLB == 1) begin
-    logic [AXI_USER_WIDTH-1:0] l2_axi4_aruser  ;
-    logic                [3:0] l2_axi4_arcache ;
-    logic                [3:0] l2_axi4_arregion;
-    logic                [3:0] l2_axi4_arqos   ;
-    logic                [2:0] l2_axi4_arprot  ;
-    logic                      l2_axi4_arlock  ;
-    logic                [1:0] l2_axi4_arburst ;
-    logic                [2:0] l2_axi4_arsize  ;
-    logic                [7:0] l2_axi4_arlen   ;
-    logic   [AXI_ID_WIDTH-1:0] l2_axi4_arid    ;
+  if (ENABLE_L2TLB) begin : gen_l2_tlb
+    user_t            l2_axi4_aruser;
+    axi_pkg::cache_t  l2_axi4_arcache;
+    axi_pkg::region_t l2_axi4_arregion;
+    axi_pkg::qos_t    l2_axi4_arqos;
+    axi_pkg::prot_t   l2_axi4_arprot;
+    logic             l2_axi4_arlock;
+    axi_pkg::burst_t  l2_axi4_arburst;
+    axi_pkg::size_t   l2_axi4_arsize;
+    axi_pkg::len_t    l2_axi4_arlen;
+    id_t              l2_axi4_arid;
 
     assign m_axi4_aruser  = l2_sending_o ? l2_axi4_aruser   : s_axi4_aruser;
     assign m_axi4_arcache = l2_sending_o ? l2_axi4_arcache  : s_axi4_arcache;
@@ -96,8 +96,8 @@ generate
     assign m_axi4_arid    = l2_sending_o ? l2_axi4_arid     : s_axi4_arid;
 
     // Buffer AXI signals in case of L1 miss
-    always @(posedge axi4_aclk or negedge axi4_arstn) begin
-      if (axi4_arstn == 1'b0) begin
+    always @(posedge axi4_aclk, negedge axi4_arstn) begin
+      if (!axi4_arstn) begin
         l2_axi4_aruser  <=  'b0;
         l2_axi4_arcache <=  'b0;
         l2_axi4_arprot  <=  'b0;
@@ -119,10 +119,10 @@ generate
     end
 
     // signal that an l1_save_i can be accepted
-    always @(posedge axi4_aclk or negedge axi4_arstn) begin
-      if (axi4_arstn == 1'b0) begin
+    always @(posedge axi4_aclk, negedge axi4_arstn) begin
+      if (!axi4_arstn) begin
         l2_available_q <= 1'b1;
-      end else if (l2_sent | l2_drop_i) begin
+      end else if (l2_sent || l2_drop_i) begin
         l2_available_q <= 1'b1;
       end else if (l1_save) begin
         l2_available_q <= 1'b0;
@@ -136,7 +136,7 @@ generate
     //    2: drop request (L2 slot is available again)
     assign l2_done_o    = l2_sent | l2_drop_i;
 
-  end else begin // !`ifdef ENABLE_L2TLB
+  end else begin : gen_no_l2_tlb
     assign m_axi4_aruser  =  s_axi4_aruser;
     assign m_axi4_arcache =  s_axi4_arcache;
     assign m_axi4_arprot  =  s_axi4_arprot;
@@ -150,7 +150,6 @@ generate
     assign l2_sending_o   = 1'b0;
     assign l2_available_q = 1'b0;
     assign l2_done_o      = 1'b0;
-  end // else: !if(ENABLE_L2TLB == 1)
-endgenerate
+  end
 
 endmodule
