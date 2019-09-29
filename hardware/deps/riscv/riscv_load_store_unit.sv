d0fc4b1df76a3b0003582b043452524928d97b09
Andreas Kurth <akurth@iis.ee.ethz.ch>
2019-09-29 07:50:17 +0200

    hardware/riscv_load_store_unit: :art: Reformat to current style guide

--- hardware/deps/riscv/riscv_load_store_unit.sv
+++ hardware/deps/riscv/riscv_load_store_unit.sv
@@ -8,68 +8,51 @@
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the
 // specific language governing permissions and limitations under the License.
 
-////////////////////////////////////////////////////////////////////////////////
-// Engineer:       Igor Loi - igor.loi@unibo.it                               //
-//                                                                            //
-// Additional contributions by:                                               //
-//                 Andreas Traber - atraber@iis.ee.ethz.ch                    //
-//                                                                            //
-// Design Name:    Load Store Unit                                            //
-// Project Name:   RI5CY                                                      //
-// Language:       SystemVerilog                                              //
-//                                                                            //
-// Description:    Load Store Unit, used to eliminate multiple access during  //
-//                 processor stalls, and to align bytes and halfwords         //
-//                                                                            //
-////////////////////////////////////////////////////////////////////////////////
+module riscv_load_store_unit (
+  input  logic        clk_i,
+  input  logic        rst_ni,
 
+  // output to data memory
+  output logic        data_req_o,
+  input  logic        data_gnt_i,
+  input  logic        data_rvalid_i,
+  input  logic        data_err_i,
 
-module riscv_load_store_unit
-(
-    input  logic         clk,
-    input  logic         rst_n,
+  output logic [31:0] data_addr_o,
+  output logic        data_we_o,
+  output logic  [3:0] data_be_o,
+  output logic [31:0] data_wdata_o,
+  input  logic [31:0] data_rdata_i,
 
-    // output to data memory
-    output logic         data_req_o,
-    input  logic         data_gnt_i,
-    input  logic         data_rvalid_i,
-    input  logic         data_err_i,
+  // signals from ex stage
+  input  logic        data_we_ex_i,         // write enable                      -> from ex stage
+  input  logic  [1:0] data_type_ex_i,       // Data type word, halfword, byte    -> from ex stage
+  input  logic [31:0] data_wdata_ex_i,      // data to write to memory           -> from ex stage
+  input  logic  [1:0] data_reg_offset_ex_i, // offset inside register for stores -> from ex stage
+  input  logic        data_sign_ext_ex_i,   // sign extension                    -> from ex stage
 
-    output logic [31:0]  data_addr_o,
-    output logic         data_we_o,
-    output logic [3:0]   data_be_o,
-    output logic [31:0]  data_wdata_o,
-    input  logic [31:0]  data_rdata_i,
+  output logic [31:0] data_rdata_ex_o,      // requested data                    -> to ex stage
+  input  logic        data_req_ex_i,        // data request                      -> from ex stage
+  input  logic [31:0] operand_a_ex_i,       // operand a from RF for address     -> from ex stage
+  input  logic [31:0] operand_b_ex_i,       // operand b from RF for address     -> from ex stage
+  input  logic        addr_useincr_ex_i,    // use a + b or just a for address   -> from ex stage
 
-    // signals from ex stage
-    input  logic         data_we_ex_i,         // write enable                      -> from ex stage
-    input  logic [1:0]   data_type_ex_i,       // Data type word, halfword, byte    -> from ex stage
-    input  logic [31:0]  data_wdata_ex_i,      // data to write to memory           -> from ex stage
-    input  logic [1:0]   data_reg_offset_ex_i, // offset inside register for stores -> from ex stage
-    input  logic         data_sign_ext_ex_i,   // sign extension                    -> from ex stage
+  input  logic        data_misaligned_ex_i, // misaligned access in last ld/st   -> from ID/EX pipeline
+  output logic        data_misaligned_o,    // misaligned access was detected    -> to controller
 
-    output logic [31:0]  data_rdata_ex_o,      // requested data                    -> to ex stage
-    input  logic         data_req_ex_i,        // data request                      -> from ex stage
-    input  logic [31:0]  operand_a_ex_i,       // operand a from RF for address     -> from ex stage
-    input  logic [31:0]  operand_b_ex_i,       // operand b from RF for address     -> from ex stage
-    input  logic         addr_useincr_ex_i,    // use a + b or just a for address   -> from ex stage
+  input  logic  [5:0] data_atop_ex_i,       // atomic instructions signal        -> from ex stage
+  output logic  [5:0] data_atop_o,          // atomic instruction signal         -> core output
 
-    input  logic         data_misaligned_ex_i, // misaligned access in last ld/st   -> from ID/EX pipeline
-    output logic         data_misaligned_o,    // misaligned access was detected    -> to controller
+  // exception signals
+  output logic        load_err_o,
+  output logic        store_err_o,
 
-    input  logic [5:0]   data_atop_ex_i,       // atomic instructions signal        -> from ex stage
-    output logic [5:0]   data_atop_o,          // atomic instruction signal         -> core output
+  // stall signal
+  output logic        lsu_ready_ex_o, // LSU ready for new data in EX stage
+  output logic        lsu_ready_wb_o, // LSU ready for new data in WB stage
 
-    // exception signals
-    output logic         load_err_o,
-    output logic         store_err_o,
-
-    // stall signal
-    output logic         lsu_ready_ex_o, // LSU ready for new data in EX stage
-    output logic         lsu_ready_wb_o, // LSU ready for new data in WB stage
-
-    input  logic         ex_valid_i,
-    output logic         busy_o
+  input  logic        ex_valid_i,
+  output logic        busy_o
 );
 
   // Calculate shift amount for strobe and data for unaligned accesses.
@@ -175,114 +158,97 @@ module riscv_load_store_unit
     end
   end
 
-  enum logic [1:0]  { IDLE, WAIT_RVALID, WAIT_RVALID_EX_STALL, IDLE_EX_STALL } CS, NS;
+  enum logic [1:0] {
+    Idle, WaitRValid, WaitRValidExStall, IdleExStall
+  } state_d, state_q;
 
-  // FSM
-  always_comb
-  begin
-    NS             = CS;
+  // Main LSU FSM
+  always_comb begin
+    state_d         = state_q;
+    data_req_o      = 1'b0;
+    lsu_ready_ex_o  = 1'b1;
+    lsu_ready_wb_o  = 1'b1;
 
-    data_req_o     = 1'b0;
-
-    lsu_ready_ex_o = 1'b1;
-    lsu_ready_wb_o = 1'b1;
-
-    case(CS)
-      // starts from not active and stays in IDLE until request was granted
-      IDLE:
-      begin
+    case (state_q)
+      Idle: begin
+        // Start here and stay in Idle until request was granted.
         data_req_o = data_req_ex_i;
-
-        if(data_req_ex_i) begin
+        if (data_req_ex_i) begin
           lsu_ready_ex_o = 1'b0;
-
-          if(data_gnt_i) begin
+          if (data_gnt_i) begin
             lsu_ready_ex_o = 1'b1;
-
-            if (ex_valid_i)
-              NS = WAIT_RVALID;
-            else
-              NS = WAIT_RVALID_EX_STALL;
+            if (ex_valid_i) begin
+              state_d = WaitRValid;
+            end else begin
+              state_d = WaitRValidExStall;
+            end
           end
         end
-      end //~ IDLE
+      end
 
-      // wait for rvalid in WB stage and send a new request if there is any
-      WAIT_RVALID:
-      begin
+      WaitRValid: begin
+        // Wait for rvalid in WB stage and send a new request if there is any.
         lsu_ready_wb_o = 1'b0;
-
         if (data_rvalid_i) begin
-          // we don't have to wait for anything here as we are the only stall
-          // source for the WB stage
-          lsu_ready_wb_o = 1'b1;
-
-          data_req_o = data_req_ex_i;
-
+          // We don't have to wait for anything here as we are the only stall source for the WB
+          // stage.
+          lsu_ready_wb_o  = 1'b1;
+          data_req_o      = data_req_ex_i;
           if (data_req_ex_i) begin
             lsu_ready_ex_o = 1'b0;
-
             if (data_gnt_i) begin
               lsu_ready_ex_o = 1'b1;
-
-              if(ex_valid_i)
-                NS = WAIT_RVALID;
-              else
-                NS = WAIT_RVALID_EX_STALL;
+              if (ex_valid_i) begin
+                state_d = WaitRValid;
+              end else begin
+                state_d = WaitRValidExStall;
+              end
             end else begin
-              NS = IDLE;
+              state_d = Idle;
             end
           end else begin
             if (data_rvalid_i) begin
-              // no request, so go to IDLE
-              NS = IDLE;
+              // No request, so go to Idle
+              state_d = Idle;
             end
           end
         end
       end
 
-      // wait for rvalid while still in EX stage
-      // we end up here when there was an EX stall, so in this cycle we just
-      // wait and don't send new requests
-      WAIT_RVALID_EX_STALL:
-      begin
+      WaitRValidExStall: begin
+        // Wait for rvalid while still in EX stage.  We end up here when there was an EX stall, so
+        // in this cycle we just wait and don't send new requests.
         data_req_o = 1'b0;
-
         if (data_rvalid_i) begin
           if (ex_valid_i) begin
-            // we are done and can go back to idle
-            // the data is safely stored already
-            NS = IDLE;
+            // We are done and can go back to Idle.  The data is safely stored already.
+            state_d = Idle;
           end else begin
-            // we have to wait until ex_stall is deasserted
-            NS = IDLE_EX_STALL;
+            // We have to wait until ex_stall is deasserted.
+            state_d = IdleExStall;
           end
         end else begin
-          // we didn't yet receive the rvalid, so we check the ex_stall
-          // signal. If we are no longer stalled we can change to the "normal"
-          // WAIT_RVALID state
-          if (ex_valid_i)
-            NS = WAIT_RVALID;
+          // We didn't yet receive the rvalid, so we check the ex_stall signal. If we are no longer
+          // stalled we can change to the "normal" WaitRValid state.
+          if (ex_valid_i) begin
+            state_d = WaitRValid;
+          end
         end
       end
 
-      IDLE_EX_STALL:
-      begin
-        // wait for us to be unstalled and then change back to IDLE state
+      IdleExStall: begin
+        // Wait for us to be unstalled and then change back to Idle state.
         if (ex_valid_i) begin
-          NS = IDLE;
+          state_d = Idle;
         end
       end
 
-      default: begin
-        NS = IDLE;
-      end
+      default: state_d = Idle;
     endcase
   end
 
-  // check for misaligned accesses that need a second memory access
-  // If one is detected, this is signaled with data_misaligned_o to
-  // the controller which selectively stalls the pipeline
+  // Check for misaligned accesses that need a second memory access.  If one is detected, this is
+  // signaled with data_misaligned_o to the controller, which selectively stalls the pipeline.
   always_comb begin
     data_misaligned_o = 1'b0;
     if (data_req_ex_i && !data_misaligned_ex_i) begin
@@ -292,7 +258,7 @@ module riscv_load_store_unit
     end
   end
 
-  // generate address from operands
+  // Generate address from operands.
   always_comb begin
     data_addr_o = operand_a_ex_i;
     if (addr_useincr_ex_i) begin
@@ -303,54 +269,52 @@ module riscv_load_store_unit
     end
   end
 
-  assign busy_o = (CS == WAIT_RVALID) || (CS == WAIT_RVALID_EX_STALL) || (CS == IDLE_EX_STALL) || (data_req_o == 1'b1);
+  assign busy_o = (state_q == WaitRValid) || (state_q == WaitRValidExStall)
+      || (state_q == IdleExStall) || (data_req_o);
 
-  // output to register file
+  // Output to register file
   assign data_rdata_ex_o = data_rvalid_i ? rdata_d : rdata_q;
 
-  // output to data interface
+  // Output to data interface
   assign data_we_o        = data_we_ex_i;
   assign data_atop_o      = data_atop_ex_i;
 
   assign load_err_o       = data_gnt_i && data_err_i && ~data_we_o;
   assign store_err_o      = data_gnt_i && data_err_i && data_we_o;
 
-  always_ff @(posedge clk, negedge rst_n) begin
-    if (!rst_n) begin
-      CS            <= IDLE;
+  always_ff @(posedge clk_i, negedge rst_ni) begin
+    if (!rst_ni) begin
       mask_q        <=   '0;
       rdata_q       <=   '0;
       resp_state_q  <= RespRegular;
       shamt_q       <=   '0;
       sign_ext_q    <= 1'b0;
+      state_q       <= Idle;
       type_q        <=   '0;
       we_q          <= 1'b0;
     end else begin
-      CS            <= NS;
       mask_q        <= mask_d;
       rdata_q       <= rdata_d;
       resp_state_q  <= resp_state_d;
       shamt_q       <= shamt_d;
       sign_ext_q    <= sign_ext_d;
+      state_q       <= state_d;
       type_q        <= type_d;
       we_q          <= we_d;
     end
   end
 
-  //////////////////////////////////////////////////////////////////////////////
   // Assertions
-  //////////////////////////////////////////////////////////////////////////////
-
   `ifndef VERILATOR
-    // make sure there is no new request when the old one is not yet completely done
-    assert property (@(posedge clk) CS == WAIT_RVALID && data_gnt_i |-> data_rvalid_i)
+    // Make sure there is no new request when the old one is not yet completely done.
+    assert property (@(posedge clk_i) state_q == WaitRValid && data_gnt_i |-> data_rvalid_i)
         else $warning("It should not be possible to get a grant without an rvalid for the last request!");
 
-    assert property (@(posedge clk) CS == IDLE |-> !data_rvalid_i)
+    assert property (@(posedge clk_i) state_q == Idle |-> !data_rvalid_i)
         else $warning("There should be no rvalid when the LSU is idle!");
 
-    // assert that the address does not contain X when request is sent
-    assert property (@(posedge clk) data_req_o |-> !$isunknown(data_addr_o))
+    // Assert that the address does not contain X when request is sent.
+    assert property (@(posedge clk_i) data_req_o |-> !$isunknown(data_addr_o))
         else $warning("There has been a data request but the address is unknown!");
   `endif
 endmodule
