e29e9db3507f1547f915acc359e662f0fdc798b2
Andreas Kurth <akurth@iis.ee.ethz.ch>
2019-10-03 13:34:15 +0200

    hardware/vsim/start_sim.sh: Quit at end of console-only simulation

--- hardware/vsim/start_sim.sh
+++ hardware/vsim/start_sim.sh
@@ -4,7 +4,7 @@ set -e
 
 if [ -z "$DISPLAY" ]; then
     # Run in console-only mode.
-    vsim-10.7b -c -do 'source run.tcl'
+    vsim-10.7b -c -do 'source run.tcl; quit'
 else
     # Run in GUI mode and silence console output.
     vsim-10.7b -do 'source run.tcl' &>/dev/null
