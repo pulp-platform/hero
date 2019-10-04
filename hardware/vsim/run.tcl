1af4e61c541c7c30162bed56e5a4cfccd317628c
Andreas Kurth <akurth@iis.ee.ethz.ch>
2019-10-04 09:28:15 +0200

    hardware/vsim: Propagate the final state of the simulation to the exit code

--- hardware/vsim/run.tcl
+++ hardware/vsim/run.tcl
@@ -6,3 +6,7 @@ source ../test/hero_tb.wave.do
 
 onfinish stop
 run -a
+
+# Set `quitCode` variable (to be used for the exit code) to `1` if the simulation terminated in an
+# unknown state (e.g., due to a `$fatal`) and to `0` otherwise.
+quietly set quitCode [expr [string match "*unknown" [runStatus -full]] ? 1 : 0]
