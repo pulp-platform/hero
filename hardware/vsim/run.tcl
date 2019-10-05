47ab193bff405c6b87246bbe0eace2964e69525c
Andreas Kurth <akurth@iis.ee.ethz.ch>
2019-10-05 16:38:38 +0200

    hardware/vsim: Load wave configuration only in non-batch mode

--- hardware/vsim/run.tcl
+++ hardware/vsim/run.tcl
@@ -2,7 +2,9 @@ vsim -voptargs="+acc" -t 1ps -warning 3009 hero_tb
 set StdArithNoWarnings 1
 set NumericStdNoWarnings 1
 
-source ../test/hero_tb.wave.do
+if { ! [batch_mode] } {
+    source ../test/hero_tb.wave.do
+}
 
 onfinish stop
 run -a
