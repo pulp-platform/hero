1394dcf9d4ae92f2613b89c1f9d6d5130a51ec6d
Koen Wolters <kwolters@student.ethz.ch>
2019-10-10 22:02:47 +0200

    format 2mm benchmark

--- openmp-examples/polybench-acc/2mm/2mm.c
+++ openmp-examples/polybench-acc/2mm/2mm.c
@@ -86,19 +86,19 @@ void kernel_2mm(int ni, int nj, int nk, int nl,
     #pragma omp for private (j, k)
     for (i = 0; i < _PB_NI; i++)
       for (j = 0; j < _PB_NJ; j++)
-  	{
-    	  tmp[i][j] = 0;
-  	  for (k = 0; k < _PB_NK; ++k)
-	    tmp[i][j] += alpha * A[i][k] * B[k][j];
-        }
+      {
+        tmp[i][j] = 0;
+        for (k = 0; k < _PB_NK; ++k)
+          tmp[i][j] += alpha * A[i][k] * B[k][j];
+      }
     #pragma omp for private (j, k)
     for (i = 0; i < _PB_NI; i++)
       for (j = 0; j < _PB_NL; j++)
-        {
-	  D[i][j] *= beta;
-	  for (k = 0; k < _PB_NJ; ++k)
-	    D[i][j] += tmp[i][k] * C[k][j];
-	}
+      {
+        D[i][j] *= beta;
+        for (k = 0; k < _PB_NJ; ++k)
+          D[i][j] += tmp[i][k] * C[k][j];
+      }
   }
   #pragma endscop
 }
