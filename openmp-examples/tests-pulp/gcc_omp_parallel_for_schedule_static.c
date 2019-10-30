9f14e5df9902f316ef3947b623a4355c36f97baa
Wei Qiu <qiuw@student.ethz.ch>
2019-10-30 19:07:50 +0100

    toolchain/openmp: Refine gcc_omp_parallel_for_schedule_static

--- openmp-examples/tests-pulp/gcc_omp_parallel_for_schedule_static.c
+++ openmp-examples/tests-pulp/gcc_omp_parallel_for_schedule_static.c
@@ -8,45 +8,28 @@ int gcc_omp_parallel_for_schedule_static (void)
   int result = 0;
   memset (buf, '\0', sizeof (buf));
 
-// #pragma omp parallel for
-//   for (i = 0; i < omp_get_num_threads(); i++){
-//     if(omp_get_thread_num()!=i){
-//       printf("Error: for loop is not executed in parallel\n");
-//       result = 1;
-//     }
-//   }
-//   printf("Parallel for is exceuted correctly\n");
-  // printf("Buffer address %8x\n",  (unsigned) buf);
-  // #pragma omp parallel for schedule (static, 3)
-  // for (i = 10; i < 54; i++)
-  //   buf[i] = 5;
-  // for (i = 0; i < 64; i++)
-  //   printf("Buffer %d: %d\n", i, buf[i]);
-  //   if (buf[i] != 5 * (i >= 10 && i < 54)){
-  //     printf("error 21 at loop-5\n");
-  //     result += 1;
-  //   }
-  // printf("success 0\n");
-  // memset (buf, '\0', sizeof (buf));
+#pragma omp parallel for
+  for (i = 0; i < omp_get_num_threads(); i++){
+    if(omp_get_thread_num()!=i){
+      printf("Error: for loop is not executed in parallel\n");
+      result = 1;
+    }
+  }
 #pragma omp parallel for schedule (static, 3) private(p)
-  // for 
   for (p = &buf[10]; p < &buf[54]; p++)
-    // printf("&p = ")
     *p = 5;
   for (i = 0; i < 64; i++)
-    printf("Buffer %d: %d\n", i, buf[i]);
     if (buf[i] != 5 * (i >= 10 && i < 54)){
-      printf("error 21 at loop-5\n");
+      printf("error 1 at gcc schedule static\n");
       result += 1;
     }
-  printf("success 1\n");
   memset (buf, '\0', sizeof (buf));
 #pragma omp parallel for schedule (static, 3)
   for (p = &buf[3]; p <= &buf[63]; p += 2)
     p[-2] = 6;
   for (i = 0; i < 64; i++)
     if (buf[i] != 6 * ((i & 1) && i <= 61)){
-      printf("error 22 at loop-5\n");
+      printf("error 2 at gcc schedule static\n");
       result += 1;
     }
   memset (buf, '\0', sizeof (buf));
@@ -55,7 +38,7 @@ int gcc_omp_parallel_for_schedule_static (void)
     p[2] = 7;
   for (i = 0; i < 64; i++)
     if (buf[i] != 7 * ((i & 3) == 2 && i >= 18 && i < 53)){
-      printf("error 23 at loop-5\n");
+      printf("error 3 at gcc schedule static\n");
       result += 1;
     }
   memset (buf, '\0', sizeof (buf));
@@ -64,7 +47,7 @@ int gcc_omp_parallel_for_schedule_static (void)
     p[2] = -7;
   for (i = 0; i < 64; i++)
     if (buf[i] != -7 * ((i & 3) == 2 && i >= 18 && i <= 42)){
-      printf("error 24 at loop-5\n");
+      printf("error 4 at gcc schedule static\n");
       result += 1;
     }
   memset (buf, '\0', sizeof (buf));
@@ -73,7 +56,7 @@ int gcc_omp_parallel_for_schedule_static (void)
     *p = 5;
   for (i = 0; i < 64; i++)
     if (buf[i] != 5 * (i >= 10 && i < 54)){
-      printf("error 25 at loop-5\n");
+      printf("error 5 at gcc schedule static\n");
       result += 1;
     }
   memset (buf, '\0', sizeof (buf));
@@ -82,7 +65,7 @@ int gcc_omp_parallel_for_schedule_static (void)
     p[-2] = 6;
   for (i = 0; i < 64; i++)
     if (buf[i] != 6 * ((i & 1) && i <= 61)){
-      printf("error 26 at loop-5\n");
+      printf("error 6 at gcc schedule static\n");
       result += 1;
     }
   memset (buf, '\0', sizeof (buf));
@@ -91,7 +74,7 @@ int gcc_omp_parallel_for_schedule_static (void)
     p[2] = 7;
   for (i = 0; i < 64; i++)
     if (buf[i] != 7 * ((i & 3) == 2 && i >= 18 && i < 53)){
-      printf("error 27 at loop-5\n");
+      printf("error 7 at at gcc schedule static\n");
       result += 1;
     }
   memset (buf, '\0', sizeof (buf));
@@ -100,13 +83,8 @@ int gcc_omp_parallel_for_schedule_static (void)
     p[2] = -7;
   for (i = 0; i < 64; i++)
     if (buf[i] != -7 * ((i & 3) == 2 && i >= 18 && i <= 42)){
-      printf("error 28 at loop-5\n");
+      printf("error 8 at gcc schedule static\n");
       result += 1;
     }
   return (result == 0);
 }
-
-// int main()
-// {
-//   printf("result:%d\n", test_ompgcc_for_schedule_static());
-// }
