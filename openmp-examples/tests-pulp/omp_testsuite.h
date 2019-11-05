0a171264e2cd9030cd43d29c00143a0dca6904e2
Wei Qiu <qiuw@student.ethz.ch>
2019-11-05 15:54:43 +0100

    tests-pulp: Delete the comment regarding the time unit in omp_testsuite.h

--- openmp-examples/tests-pulp/omp_testsuite.h
+++ openmp-examples/tests-pulp/omp_testsuite.h
@@ -12,7 +12,6 @@
 #define LOOPCOUNT 100 /* Number of iterations to slit amongst threads */
 #define REPETITIONS 1 /* Number of times to run each test */
 
-/* following times are in seconds */
 #define SLEEPTIME 1000
 
 /* Definitions for tasks                                  */
