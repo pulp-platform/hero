55ac2b739ddc5f5a65b3ee58ec7170bf88453d0f
Koen Wolters <kwolters@student.ethz.ch>
2019-11-07 17:36:00 +0100

    buildroot: move external mount to overlay

--- board/common/post_build.sh
+++ board/common/post_build.sh
@@ -1,2 +1,11 @@
 echo "Removing NFS init.d script"
 rm -f $1/etc/init.d/S60nfs
+
+echo "Installing custom mount script"
+EXT_MOUNT=$(grep BR2_HERO_EXT_MOUNT ${BR2_CONFIG} | sed -e 's/.*=//' -e 's/^"//' -e 's/"$//')
+echo $EXTMOUNT
+if [ -z "$EXT_MOUNT" ]; then
+  rm $1/etc/init.d/S99extroot
+else
+  sed -i "s|EXTERNAL_MOUNT_POINT|${EXT_MOUNT}|" $1/etc/init.d/S99extroot
+fi
