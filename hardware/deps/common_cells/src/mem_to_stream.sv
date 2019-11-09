965d6ea8fa25790f31ee9c73dbf5cb1d2301b119
Andreas Kurth <akurth@iis.ee.ethz.ch>
2019-11-09 17:45:53 +0100

    hardware/mem_to_stream: Allow `BufDepth` of 0

--- hardware/deps/common_cells/src/mem_to_stream.sv
+++ hardware/deps/common_cells/src/mem_to_stream.sv
@@ -14,7 +14,10 @@ module mem_to_stream #(
   parameter type mem_req_t = logic,
   parameter type mem_resp_t = logic,
   // Number of buffered responses (fall-through, thus no additional latency).  This defines the
-  // maximum number of outstanding requests on the memory interface and must be >= 1.
+  // maximum number of outstanding requests on the memory interface.  If the attached memory
+  // responds in the same cycle a request is applied, this MUST be 0.  If the attached memory
+  // responds at least one cycle after a request, this MUST be >= 1 and should be equal to the
+  // response latency of the memory to saturate bandwidth.
   parameter int unsigned BufDepth = 1
 ) (
   input  logic      clk_i,
@@ -44,68 +47,81 @@ module mem_to_stream #(
   logic buf_ready,
         req_ready;
 
-  // Count number of outstanding requests.
-  always_comb begin
-    cnt_d = cnt_q;
-    if (req_valid_i && req_ready_o) begin
-      cnt_d++;
+  if (BufDepth > 0) begin : gen_buf
+    // Count number of outstanding requests.
+    always_comb begin
+      cnt_d = cnt_q;
+      if (req_valid_i && req_ready_o) begin
+        cnt_d++;
+      end
+      if (resp_valid_o && resp_ready_i) begin
+        cnt_d--;
+      end
     end
-    if (resp_valid_o && resp_ready_i) begin
-      cnt_d--;
+
+    // Can issue another request if the counter is not at its limit or a response is delivered in
+    // the current cycle.
+    assign req_ready = (cnt_q < BufDepth) | (resp_valid_o & resp_ready_i);
+
+    // Control request and memory request interface handshakes.
+    assign req_ready_o = mem_req_ready_i & req_ready;
+    assign mem_req_valid_o = req_valid_i & req_ready;
+
+    // Buffer responses.
+    stream_fifo #(
+      .FALL_THROUGH (1'b1),
+      .DEPTH        (BufDepth),
+      .T            (mem_resp_t)
+    ) i_resp_buf (
+      .clk_i,
+      .rst_ni,
+      .flush_i    (1'b0),
+      .testmode_i (1'b0),
+      .data_i     (mem_resp_i),
+      .valid_i    (mem_resp_valid_i),
+      .ready_o    (buf_ready),
+      .data_o     (resp_o),
+      .valid_o    (resp_valid_o),
+      .ready_i    (resp_ready_i),
+      .usage_o    (/* unused */)
+    );
+
+    // Register
+    always_ff @(posedge clk_i, negedge rst_ni) begin
+      if (!rst_ni) begin
+        cnt_q <= '0;
+      end else begin
+        cnt_q <= cnt_d;
+      end
     end
+
+  end else begin : gen_no_buf
+    // Control request, memory request, and response interface handshakes.
+    assign mem_req_valid_o = req_valid_i;
+    assign resp_valid_o = mem_req_valid_o & mem_req_ready_i & mem_resp_valid_i;
+    assign req_ready_o = resp_ready_i;
+
+    // Forward responses.
+    assign resp_o = mem_resp_i;
   end
-
-  // Can issue another request if the counter is not at its limit or a response is delivered in the
-  // current cycle.
-  assign req_ready = (cnt_q < BufDepth) | (resp_valid_o & resp_ready_i);
-
-  // Control request and memory request interface handshakes.
-  assign req_ready_o = mem_req_ready_i & req_ready;
-  assign mem_req_valid_o = req_valid_i & req_ready;
 
   // Forward requests.
   assign mem_req_o = req_i;
 
-  // Buffer responses.
-  stream_fifo #(
-    .FALL_THROUGH (1'b1),
-    .DEPTH        (BufDepth),
-    .T            (mem_resp_t)
-  ) i_resp_buf (
-    .clk_i,
-    .rst_ni,
-    .flush_i    (1'b0),
-    .testmode_i (1'b0),
-    .data_i     (mem_resp_i),
-    .valid_i    (mem_resp_valid_i),
-    .ready_o    (buf_ready),
-    .data_o     (resp_o),
-    .valid_o    (resp_valid_o),
-    .ready_i    (resp_ready_i),
-    .usage_o    (/* unused */)
-  );
-
-  // Register
-  always_ff @(posedge clk_i, negedge rst_ni) begin
-    if (!rst_ni) begin
-      cnt_q <= '0;
-    end else begin
-      cnt_q <= cnt_d;
-    end
-  end
-
   // Assertions
   `ifndef VERILATOR
   `ifndef TARGET_SYNTHESIS
-    initial begin
-      assert (BufDepth >= 1) else $fatal("Buffer depth must be at least 1!");
+    if (BufDepth > 0) begin : gen_buf_asserts
+      assert property (@(posedge clk_i) mem_resp_valid_i |-> buf_ready)
+        else $error("Memory response lost!");
+      assert property (@(posedge clk_i) cnt_q == '0 |=> cnt_q != '1)
+        else $error("Counter underflowed!");
+      assert property (@(posedge clk_i) cnt_q == BufDepth |=> cnt_q != BufDepth + 1)
+        else $error("Counter overflowed!");
+    end else begin : gen_no_buf_asserts
+      assume property (@(posedge clk_i) mem_req_valid_o & mem_req_ready_i |-> mem_resp_valid_i)
+        else $error("Without BufDepth = 0, the memory must respond in the same cycle!");
     end
-    assert property (@(posedge clk_i) mem_resp_valid_i |-> buf_ready)
-      else $error("Memory response lost!");
-    assert property (@(posedge clk_i) cnt_q == '0 |=> cnt_q != '1)
-      else $error("Counter underflowed!");
-    assert property (@(posedge clk_i) cnt_q == BufDepth |=> cnt_q != BufDepth + 1)
-      else $error("Counter overflowed!");
   `endif
   `endif
 
