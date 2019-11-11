6040c2629ddad64e9b34c2e69e112d35f485ecdf
Andreas Kurth <akurth@iis.ee.ethz.ch>
2019-11-11 18:58:08 +0100

    hardware: Add dummy PL design for ZCU102

---
+++ hardware/fpga/zcu102/dummy/project_1.srcs/sources_1/bd/design_1/hdl/design_1_wrapper.v
@@ -0,0 +1,18 @@
+//Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2019.1.1 (lin64) Build 2580384 Sat Jun 29 08:04:45 MDT 2019
+//Date        : Sun Nov 10 19:01:45 2019
+//Host        : citadel.ee.ethz.ch running 64-bit CentOS Linux release 7.4.1708 (Core)
+//Command     : generate_target design_1_wrapper.bd
+//Design      : design_1_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module design_1_wrapper
+   ();
+
+
+  design_1 design_1_i
+       ();
+endmodule
