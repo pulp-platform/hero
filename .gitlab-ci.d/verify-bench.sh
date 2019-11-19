6701d6f0711efba8ef84248216cd1cfc9d89d85e
Koen Wolters <kwolters@student.ethz.ch>
2019-11-19 15:02:12 +0100

    ci: verify polybench output

---
+++ .gitlab-ci.d/verify-bench.sh
@@ -0,0 +1,20 @@
+curdir=$(dirname "$(readlink -f "$0")")
+cd $curdir/../hardware/vsim
+../test/gen_slm_files.sh polybench-acc/$1
+output=$(mktemp)
+DISPLAY= ./start_sim.sh | tee $output
+
+simout=$(cat $output | fgrep '# [0,0]' | tail -n +2 | sed -e 's/# \[0,0\] //')
+expout=$(cat "$curdir/../openmp-examples/polybench-acc/$1/$1.exp")
+rm $output
+if [ "$simout" = "$expout" ]; then
+  echo "> OUTPUT: OK";
+  exit 0
+else
+  echo "> OUTPUT: WRONG!"
+  echo "--- OUT ---"
+  echo $simout
+  echo "--- EXP ---"
+  echo $expout
+  exit 1
+fi
