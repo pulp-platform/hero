63832b924e81f94833b7610694d968b77f3b7e1e
Andreas Kurth <akurth@iis.ee.ethz.ch>
2019-11-25 18:40:09 +0100

    hardware: Reduce size of l1 to 128 KiB and L2 to 64 KiB

--- hardware/src/pulp_cluster_cfg_pkg.sv
+++ hardware/src/pulp_cluster_cfg_pkg.sv
@@ -31,9 +31,9 @@ package automatic pulp_cluster_cfg_pkg;
   localparam int unsigned ICACHE_SIZE = 4096; // [B], must be a power of 2
   // -- TCDM
   localparam int unsigned N_TCDM_BANKS = 2*N_CORES; // must be a power of 2
-  localparam int unsigned TCDM_SIZE = 256*1024; // [B], must be a power of 2
+  localparam int unsigned TCDM_SIZE = 128*1024; // [B], must be a power of 2
   // -- L2 Memory (not inside cluster)
-  localparam int unsigned L2_SIZE = 512*1024; // [B], must be a power of 2
+  localparam int unsigned L2_SIZE = 64*1024; // [B], must be a power of 2
 
   typedef logic      [AXI_AW-1:0] addr_t;
   typedef logic             [5:0] cluster_id_t;
