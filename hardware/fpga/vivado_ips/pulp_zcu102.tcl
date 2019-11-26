58f3620485aa9fac9654f3a2de3ab6d2fe7e1b2d
Andreas Kurth <akurth@iis.ee.ethz.ch>
2019-11-26 20:18:36 +0100

    hardware/vivado_ips: Initialize `pulp_zcu102` IP

---
+++ hardware/fpga/vivado_ips/pulp_zcu102.tcl
@@ -0,0 +1,25 @@
+create_project pulp_zcu102 ./pulp_zcu102 -part xczu9eg-ffvb1156-2-e
+set_property board_part xilinx.com:zcu102:part0:3.3 [current_project]
+
+source ./define_srcs.tcl
+add_files -fileset [current_fileset] ./pulp_zcu102.v
+add_files -fileset constrs_1 ./pulp_zcu102.xdc
+set_property top pulp_zcu102 [current_fileset]
+
+synth_design -rtl -name rtl_1
+
+ipx::package_project -root_dir . -vendor ethz.ch -library user -taxonomy /UserIP -import_files \
+  -set_current false
+ipx::unload_core ./component.xml
+ipx::edit_ip_in_project -upgrade true -name tmp_edit_project -directory . ./component.xml
+set_property core_revision 2 [ipx::current_core]
+ipx::update_source_project_archive -component [ipx::current_core]
+ipx::create_xgui_files [ipx::current_core]
+ipx::update_checksums [ipx::current_core]
+ipx::save_core [ipx::current_core]
+ipx::move_temp_component_back -component [ipx::current_core]
+close_project -delete
+
+set_property ip_repo_paths . [current_project]
+update_ip_catalog
+close_project
---
+++ hardware/fpga/vivado_ips/pulp_zcu102.xdc
@@ -0,0 +1,1 @@
+set_false_path -through [get_pins {i_bound/i_bound/gen_clusters[0].gen_cluster_sync.i_cluster/ref_clk_i}]
