8f90cf6c34cc6240422a8700ccb7951ab96fca72
Andreas Kurth <akurth@iis.ee.ethz.ch>
2019-12-02 16:09:19 +0100

    hardware/pulp_zcu102: Improve IP initialization

--- hardware/fpga/vivado_ips/pulp_zcu102.tcl
+++ hardware/fpga/vivado_ips/pulp_zcu102.tcl
@@ -1,19 +1,17 @@
 create_project pulp_zcu102 ./pulp_zcu102 -part xczu9eg-ffvb1156-2-e
-set_property board_part xilinx.com:zcu102:part0:3.3 [current_project]
+set_property part xczu9eg-ffvb1156-2-e [current_project]
 
 source ./define_srcs.tcl
-add_files -fileset [current_fileset] ./pulp_zcu102.v
-add_files -fileset constrs_1 -norecurse {./pulp_zcu102_synth.xdc ./pulp_zcu102_impl.xdc}
+add_files -norecurse -fileset [current_fileset] ./pulp_zcu102.v
+add_files -norecurse -fileset constrs_1 {./pulp_zcu102_synth.xdc ./pulp_zcu102_impl.xdc}
 set_property used_in_implementation false [get_files ./pulp_zcu102_synth.xdc]
 set_property used_in_synthesis false [get_files ./pulp_zcu102_impl.xdc]
 set_property top pulp_zcu102 [current_fileset]
 
 synth_design -rtl -name rtl_1
 
-ipx::package_project -root_dir . -vendor ethz.ch -library user -taxonomy /UserIP -import_files \
-  -set_current false
-ipx::unload_core ./component.xml
-ipx::edit_ip_in_project -upgrade true -name tmp_edit_project -directory . ./component.xml
+ipx::package_project -root_dir . -vendor ethz.ch -library user -taxonomy /UserIP \
+  -set_current true
 
 # Ports and Interfaces
 ## Clock
@@ -186,9 +184,4 @@ ipx::update_source_project_archive -component [ipx::current_core]
 ipx::create_xgui_files [ipx::current_core]
 ipx::update_checksums [ipx::current_core]
 ipx::save_core [ipx::current_core]
-ipx::move_temp_component_back -component [ipx::current_core]
-close_project -delete
-
-set_property ip_repo_paths . [current_project]
-update_ip_catalog
 close_project
