856b87e9ead91e7f5d89b5fc6a68c2037bc19b18
Andreas Kurth <akurth@iis.ee.ethz.ch>
2019-12-06 11:19:57 +0100

    polybench-acc/covariance: Define extramini dataset, use it by default, and use int as data type

--- openmp-examples/polybench-acc/covariance/covariance.h
+++ openmp-examples/polybench-acc/covariance/covariance.h
@@ -9,13 +9,18 @@
 # define COVARIANCE_H
 
 /* Default to STANDARD_DATASET. */
-# if !defined(MINI_DATASET) && !defined(SMALL_DATASET) && !defined(LARGE_DATASET) && !defined(EXTRALARGE_DATASET)
-#  define STANDARD_DATASET
+# if !defined(MINI_DATASET) && !defined(SMALL_DATASET) && !defined(STANDARD_DATASET) && !defined(LARGE_DATASET) && !defined(EXTRALARGE_DATASET)
+#  define EXTRAMINI_DATASET
 # endif
 
 /* Do not define anything if the user manually defines the size. */
 # if !defined(N) && !defined(M)
 /* Define the possible dataset sizes. */
+#  ifdef EXTRAMINI_DATASET
+#   define N 8
+#   define M 8
+#  endif
+
 #  ifdef MINI_DATASET
 #   define N 32
 #   define M 32
@@ -46,8 +51,8 @@
 # define _PB_M POLYBENCH_LOOP_BOUND(M,m)
 
 # ifndef DATA_TYPE
-#  define DATA_TYPE double
-#  define DATA_PRINTF_MODIFIER "%0.2lf "
+#  define DATA_TYPE int
+#  define DATA_PRINTF_MODIFIER "%d "
 # endif
 
 
