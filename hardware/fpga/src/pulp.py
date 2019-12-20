9e12abaf01e512a67e4d1dd49139ab5045a8a3f4
Andreas Kurth <akurth@iis.ee.ethz.ch>
2019-12-20 10:10:35 +0100

    hardware/pulp_template: Reduce ID width to PL to accommodate interconnect

--- hardware/fpga/src/pulp.py
+++ hardware/fpga/src/pulp.py
@@ -12,7 +12,7 @@ pulp_template = Template(filename='pulp.template_v')
 string = pulp_template.render(
     target='xilzu9eg', n_clusters=n_clusters,
     aw=64, dw=128, iw=6+clog2(n_clusters+1), uw=4,
-    aw_pl2ps=49, iw_pl2ps=6, uw_pl2ps=1,
+    aw_pl2ps=49, iw_pl2ps=5, uw_pl2ps=1,
     aw_ps2pl=40, iw_ps2pl=17, uw_ps2pl=16,
     aw_lite=32, dw_lite=32,
 )
