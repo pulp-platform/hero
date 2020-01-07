1d3159f5818972ee848aeb91752e1213c97357d3
Andreas Kurth <akurth@iis.ee.ethz.ch>
2020-01-07 06:47:25 +0100

    fixup! Add package script for Huawei delivery

--- pack_huawei.sh
+++ pack_huawei.sh
@@ -4,12 +4,17 @@ set -e
 
 readonly SRC="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 
+# Helper function to copy git-indexed files.
+copy_git_files() {
+  git ls-files -z -- $@ | rsync -av --files-from=- -0 . "$TMP_DST/"
+}
+
 # Create temporary destination directory.
 readonly TMP_DST="$(mktemp -d)"
 
 # Hardware: Copy to destination.
 cd "$SRC"
-git ls-files -z -- hardware | rsync -av --files-from=- -0 . "$TMP_DST/"
+copy_git_files hardware
 
 # Hardware: Prepare simulation script.
 cd "$TMP_DST/hardware"
@@ -18,6 +23,14 @@ make vsim/compile.tcl
 # Hardware: Remove top-level Makefile, Bender binary, and Bender from compile script.
 rm Makefile bender
 sed -i -e 's|make -C .. vsim/compile.tcl||' vsim/compile.sh
+
+# Add SLM Converter.
+mkdir -p "$TMP_DST/install/bin"
+cp ~andkurt/bin/slm_conv-0.3 "$TMP_DST/install/bin/slm_conv"
+
+# Toolchain and Makefile: copy to destination.
+cd "$SRC"
+copy_git_files toolchain Makefile
 
 # Create archive from temporary destination directory.
 tar -C "$TMP_DST" -czf "$SRC/hero_huawei.tar.gz" .
