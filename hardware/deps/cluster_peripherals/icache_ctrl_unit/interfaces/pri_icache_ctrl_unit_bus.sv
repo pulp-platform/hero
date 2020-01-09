b65988111495bd4c5691151ce3f33c19586812d3
Andreas Kurth <akurth@iis.ee.ethz.ch>
2020-01-09 11:16:18 +0100

    hardware/pri_icache_ctrl_unit_bus: Add `ctrl_cong_count`

--- hardware/deps/cluster_peripherals/icache_ctrl_unit/interfaces/pri_icache_ctrl_unit_bus.sv
+++ hardware/deps/cluster_peripherals/icache_ctrl_unit/interfaces/pri_icache_ctrl_unit_bus.sv
@@ -17,17 +17,20 @@ interface PRI_ICACHE_CTRL_UNIT_BUS;
   logic [31:0] ctrl_hit_count;
   logic [31:0] ctrl_trans_count;
   logic [31:0] ctrl_miss_count;
+  logic [31:0] ctrl_cong_count;
   logic        ctrl_clear_regs;
   logic        ctrl_enable_regs;
 
   modport Master (
     output bypass_req, flush_req, ctrl_clear_regs, ctrl_enable_regs,
-    input  bypass_ack, flush_ack, ctrl_hit_count, ctrl_trans_count, ctrl_miss_count
+    input  bypass_ack, flush_ack, ctrl_hit_count, ctrl_trans_count, ctrl_miss_count,
+           ctrl_cong_count
   );
 
   modport Slave (
     input  bypass_req, flush_req, ctrl_clear_regs, ctrl_enable_regs,
-    output bypass_ack, flush_ack, ctrl_hit_count, ctrl_trans_count, ctrl_miss_count
+    output bypass_ack, flush_ack, ctrl_hit_count, ctrl_trans_count, ctrl_miss_count,
+           ctrl_cong_count
   );
 
 endinterface
