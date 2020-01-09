2216a8dc5a2e458e07f293ae602295c2fb19fdf2
Andreas Kurth <akurth@iis.ee.ethz.ch>
2020-01-09 07:32:51 +0100

    huawei/pulp_cluster_package: Merge with HERO

--- hardware/deps/huawei/rtl/pulp_cluster/packages/pulp_cluster_package.sv
+++ hardware/deps/huawei/rtl/pulp_cluster/packages/pulp_cluster_package.sv
@@ -29,13 +29,8 @@ package pulp_cluster_package;
   parameter SPER_HWPE_ID     = 4;
   parameter SPER_ICACHE_CTRL = 5;
   parameter SPER_DMA_CL_ID   = 6;
-  parameter SPER_DMA_FC_ID   = 7;
-  parameter SPER_DECOMP_ID   = 8; 
-  parameter SPER_EXT_ID      = 9;
-  
-  // if set to 1, then instantiate APU in the cluster
- // parameter APU_CLUSTER = 0;
-  
+  parameter SPER_EXT_ID      = 7;
+  parameter SPER_DECOMP_ID   = 8;
   // // if set to 1, the 0x0000_0000 to 0x0040_0000 is the alias of the current cluster address space (eg cluster 0 is from  0x1000_0000 to 0x1040_0000)
   // parameter CLUSTER_ALIAS = 1;
   
