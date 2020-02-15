0af5eb860a73b24bfdfcec61958162f6091462a8
Andreas Kurth <akurth@iis.ee.ethz.ch>
2020-02-15 16:07:28 +0100

    hardware/pulp_ooc: Remove `rab_conf` ports unsupported in AXI4-Lite

--- hardware/test/pulp_tb.sv
+++ hardware/test/pulp_tb.sv
@@ -46,11 +46,12 @@ module pulp_tb #(
   typedef pulp_pkg::lite_addr_t axi_lite_addr_t;
   typedef pulp_pkg::lite_data_t axi_lite_data_t;
   typedef pulp_pkg::lite_strb_t axi_lite_strb_t;
-  `AXI_LITE_TYPEDEF_AX_CHAN_T(  axi_lite_ax_t,    axi_lite_addr_t, axi_id_t, axi_user_t);
-  `AXI_LITE_TYPEDEF_W_CHAN_T(   axi_lite_w_t,     axi_lite_data_t, axi_lite_strb_t, axi_user_t);
-  `AXI_LITE_TYPEDEF_B_CHAN_T(   axi_lite_b_t,     axi_id_t, axi_user_t);
-  `AXI_LITE_TYPEDEF_R_CHAN_T(   axi_lite_r_t,     axi_lite_data_t, axi_id_t, axi_user_t);
-  `AXI_LITE_TYPEDEF_REQ_T(      axi_lite_req_t,   axi_lite_ax_t, axi_lite_w_t);
+  `AXI_LITE_TYPEDEF_AW_CHAN_T(  axi_lite_aw_t,    axi_lite_addr_t);
+  `AXI_LITE_TYPEDEF_W_CHAN_T(   axi_lite_w_t,     axi_lite_data_t, axi_lite_strb_t);
+  `AXI_LITE_TYPEDEF_B_CHAN_T(   axi_lite_b_t);
+  `AXI_LITE_TYPEDEF_AR_CHAN_T(  axi_lite_ar_t,    axi_lite_addr_t);
+  `AXI_LITE_TYPEDEF_R_CHAN_T(   axi_lite_r_t,     axi_lite_data_t);
+  `AXI_LITE_TYPEDEF_REQ_T(      axi_lite_req_t,   axi_lite_aw_t, axi_lite_w_t, axi_lite_ar_t);
   `AXI_LITE_TYPEDEF_RESP_T(     axi_lite_resp_t,  axi_lite_b_t, axi_lite_r_t);
 
   logic clk,
