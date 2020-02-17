b3a498aa28d065198d65df74fb1db4edb887f394
Andreas Kurth <akurth@iis.ee.ethz.ch>
2020-02-17 13:57:01 +0100

    hardware/gf22fdx: Update `axi` elaboration script

--- hardware/gf22fdx/syn/scripts/analyze_auto/ips/axi.tcl
+++ hardware/gf22fdx/syn/scripts/analyze_auto/ips/axi.tcl
@@ -2,35 +2,40 @@ puts "${Green}Analyzing axi ${NC}"
 
 puts "${Green}--> compile axi${NC}"
 
-analyze -format sverilog  -work work ${IPS_PATH}/axi/src/axi_pkg.sv
-analyze -format sverilog  -work work ${IPS_PATH}/axi/src/axi_address_width_converter.sv
-analyze -format sverilog  -work work ${IPS_PATH}/axi/src/axi_demux.sv
-analyze -format sverilog  -work work ${IPS_PATH}/axi/src/axi_intf.sv
-analyze -format sverilog  -work work ${IPS_PATH}/axi/src/axi_mux.sv
-analyze -format sverilog  -work work ${IPS_PATH}/axi/src/axi_perf_mon.sv
-analyze -format sverilog  -work work ${IPS_PATH}/axi/src/axi_address_resolver.sv
-analyze -format sverilog  -work work ${IPS_PATH}/axi/src/axi_arbiter.sv
-analyze -format sverilog  -work work ${IPS_PATH}/axi/src/axi_atop_filter.sv
-analyze -format sverilog  -work work ${IPS_PATH}/axi/src/axi_burst_splitter.sv
-analyze -format sverilog  -work work ${IPS_PATH}/axi/src/axi_bypass.sv
-analyze -format sverilog  -work work ${IPS_PATH}/axi/src/axi_cdc.sv
-analyze -format sverilog  -work work ${IPS_PATH}/axi/src/axi_cut.sv
-analyze -format sverilog  -work work ${IPS_PATH}/axi/src/axi_data_downsize.sv
-analyze -format sverilog  -work work ${IPS_PATH}/axi/src/axi_data_upsize.sv
-analyze -format sverilog  -work work ${IPS_PATH}/axi/src/axi_delayer.sv
-analyze -format sverilog  -work work ${IPS_PATH}/axi/src/axi_join.sv
-analyze -format sverilog  -work work ${IPS_PATH}/axi/src/axi_lite_cut.sv
-analyze -format sverilog  -work work ${IPS_PATH}/axi/src/axi_lite_join.sv
-analyze -format sverilog  -work work ${IPS_PATH}/axi/src/axi_lite_to_axi.sv
-analyze -format sverilog  -work work ${IPS_PATH}/axi/src/axi_modify_address.sv
-analyze -format sverilog  -work work ${IPS_PATH}/axi/src/axi_read_burst_buffer.sv
-analyze -format sverilog  -work work ${IPS_PATH}/axi/src/axi_to_axi_lite.sv
-analyze -format sverilog  -work work ${IPS_PATH}/axi/src/axi_bypass_wrap.sv
-analyze -format sverilog  -work work ${IPS_PATH}/axi/src/axi_data_width_converter.sv
-analyze -format sverilog  -work work ${IPS_PATH}/axi/src/axi_id_remap.sv
-analyze -format sverilog  -work work ${IPS_PATH}/axi/src/axi_lite_multicut.sv
-analyze -format sverilog  -work work ${IPS_PATH}/axi/src/axi_lite_xbar.sv
-analyze -format sverilog  -work work ${IPS_PATH}/axi/src/axi_multicut.sv
+analyze -format sv -work work \
+    -define { \
+        TARGET_SYNOPSYS \
+        TARGET_SYNTHESIS \
+    } \
+    [list \
+        "${IPS_PATH}/axi/src/axi_pkg.sv" \
+        "${IPS_PATH}/axi/src/axi_intf.sv" \
+        "${IPS_PATH}/axi/src/axi_perf_mon.sv" \
+        "${IPS_PATH}/axi/src/axi_atop_filter.sv" \
+        "${IPS_PATH}/axi/src/axi_cdc.sv" \
+        "${IPS_PATH}/axi/src/axi_cut.sv" \
+        "${IPS_PATH}/axi/src/axi_data_downsize.sv" \
+        "${IPS_PATH}/axi/src/axi_data_upsize.sv" \
+        "${IPS_PATH}/axi/src/axi_delayer.sv" \
+        "${IPS_PATH}/axi/src/axi_demux.sv" \
+        "${IPS_PATH}/axi/src/axi_id_remap.sv" \
+        "${IPS_PATH}/axi/src/axi_id_prepend.sv" \
+        "${IPS_PATH}/axi/src/axi_join.sv" \
+        "${IPS_PATH}/axi/src/axi_lite_demux.sv" \
+        "${IPS_PATH}/axi/src/axi_lite_join.sv" \
+        "${IPS_PATH}/axi/src/axi_lite_mux.sv" \
+        "${IPS_PATH}/axi/src/axi_lite_to_apb.sv" \
+        "${IPS_PATH}/axi/src/axi_lite_to_axi.sv" \
+        "${IPS_PATH}/axi/src/axi_modify_address.sv" \
+        "${IPS_PATH}/axi/src/axi_mux.sv" \
+        "${IPS_PATH}/axi/src/axi_read_burst_buffer.sv" \
+        "${IPS_PATH}/axi/src/axi_to_axi_lite.sv" \
+        "${IPS_PATH}/axi/src/axi_data_width_converter.sv" \
+        "${IPS_PATH}/axi/src/axi_decerr_slv.sv" \
+        "${IPS_PATH}/axi/src/axi_multicut.sv" \
+        "${IPS_PATH}/axi/src/axi_lite_xbar.sv" \
+        "${IPS_PATH}/axi/src/axi_xbar.sv" \
+    ]
 
 analyze -format sverilog  -work work ${IPS_PATH}/axi_riscv_atomics/src/axi_res_tbl.sv
 analyze -format sverilog  -work work ${IPS_PATH}/axi_riscv_atomics/src/axi_riscv_amos_alu.sv
