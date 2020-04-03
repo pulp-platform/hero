c918d0351faa6b567ff521e00eb787e7011b683d
Andreas Kurth <akurth@iis.ee.ethz.ch>
2020-04-03 17:56:56 +0200

    pack_siemens: Fix working directory

--- pack_siemens.sh
+++ pack_siemens.sh
@@ -72,18 +72,18 @@ cd "$SRC"
 copy_git_files Config.in board buildroot configs external.desc external.mk package petalinux
 
 # Bitstreams: import from HERO builds.
+cd ~hero # make sure this directory is mounted
 cd "$TMP_DST"
 mkdir bitstreams
-cd ~hero # make sure this directory is mounted
 cp ~hero/bitstreams/siemens-m04-deliverable/* bitstreams/
 
 # Set PULP bitstream as default for Buildroot.
 echo 'BR2_HERO_BITSTREAM="$(BR2_EXTERNAL_HERO_PATH)/bitstreams/pulp.bit"' >> "$TMP_DST/local.cfg"
 
 # SD card images: import from HERO builds.
+cd ~hero # make sure this directory is mounted
 cd "$TMP_DST"
 mkdir images
-cd ~hero # make sure this directory is mounted
 cp -r ~hero/images/siemens-m04-deliverable/* images/
 
 # Create archive from temporary destination directory.
