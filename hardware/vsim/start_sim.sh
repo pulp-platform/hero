2b057b182d9b34c71581745264b429ff79c2d4f3
bluew <bluewww@users.noreply.github.com>
2020-04-08 03:47:05 +0000

    hardware/vsim: Do not silence stdout when running in gui mode

--- hardware/vsim/start_sim.sh
+++ hardware/vsim/start_sim.sh
@@ -6,6 +6,7 @@ if [ -n "$CI" -o -z "$DISPLAY" ]; then
     # Run in console-only mode.
     vsim-10.7b -c -do 'source run.tcl; quit -code $quitCode'
 else
-    # Run in GUI mode and silence console output.
-    vsim-10.7b -do 'source run.tcl' &>/dev/null
+    # Run in GUI mode
+    # Don't silence stdout because there might be useful stdout from dpi libraries
+    vsim-10.7b -do 'source run.tcl'
 fi
