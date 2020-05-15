4f60b1278d8bbeec4cacae6026640dfe023a27fd
Andreas Kurth <akurth@iis.ee.ethz.ch>
2020-05-15 22:42:13 +0200

    hardware/axi2mem: Improve tool compatibility by splitting files to one module per file

--- hardware/gf22fdx/synopsys/scripts/analyze_auto/ips/axi2mem.tcl
+++ hardware/gf22fdx/synopsys/scripts/analyze_auto/ips/axi2mem.tcl
@@ -3,3 +3,4 @@ puts "${Green}Analyzing axi2mem ${NC}"
 puts "${Green}--> compile axi2mem${NC}"
 analyze -format sverilog  -work work ${IPS_PATH}/axi2mem/src/axi_to_mem.sv
 analyze -format sverilog  -work work ${IPS_PATH}/axi2mem/src/axi_to_mem_banked.sv
+analyze -format sverilog  -work work ${IPS_PATH}/axi2mem/src/axi_to_mem_intf.sv
