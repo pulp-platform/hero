b8c866ce729190fbc68b81f735a1965ac56b6a39
Angelo Garofalo <angelo.garofalo@unibo.it>
2020-05-15 21:45:03 +0200

    hardware/scm: Remove unused BIST

--- hardware/deps/scm/latch_scm/register_file_1r_1w_test_wrap.sv
+++ hardware/deps/scm/latch_scm/register_file_1r_1w_test_wrap.sv
@@ -27,49 +27,9 @@ module register_file_1r_1w_test_wrap
     // Write port
     input  logic                                  WriteEnable,
     input  logic [ADDR_WIDTH-1:0]                 WriteAddr,
-    input  logic [DATA_WIDTH-1:0]                 WriteData,
+    input  logic [DATA_WIDTH-1:0]                 WriteData
 
-    // BIST ENABLE
-    input  logic                                  BIST,
-    //BIST ports
-    input  logic                                  CSN_T,
-    input  logic                                  WEN_T,
-    input  logic [ADDR_WIDTH-1:0]                 A_T,
-    input  logic [DATA_WIDTH-1:0]                 D_T,
-    output logic [DATA_WIDTH-1:0]                 Q_T
 );
-
-   logic                         ReadEnable_muxed;
-   logic [ADDR_WIDTH-1:0]        ReadAddr_muxed;
-
-   logic                         WriteEnable_muxed;
-   logic [ADDR_WIDTH-1:0]        WriteAddr_muxed;
-   logic [DATA_WIDTH-1:0]        WriteData_muxed;
-
-
-   always_comb
-   begin
-      if(BIST)
-      begin
-         ReadEnable_muxed  = (( CSN_T == 1'b0 ) && ( WEN_T == 1'b1));
-         ReadAddr_muxed    = A_T;
-
-         WriteEnable_muxed = (( CSN_T == 1'b0 ) && ( WEN_T == 1'b0));
-         WriteAddr_muxed   = A_T;
-         WriteData_muxed   = D_T;
-      end
-      else
-      begin
-         ReadEnable_muxed  = ReadEnable;
-         ReadAddr_muxed    = ReadAddr;
-
-         WriteEnable_muxed = WriteEnable;
-         WriteAddr_muxed   = WriteAddr;
-         WriteData_muxed   = WriteData;
-      end
-   end
-
-   assign Q_T = ReadData;
 
    register_file_1r_1w
    #(
@@ -82,13 +42,13 @@ module register_file_1r_1w_test_wrap
 
       .test_en_i,
 
-      .ReadEnable   ( ReadEnable_muxed  ),
-      .ReadAddr     ( ReadAddr_muxed    ),
-      .ReadData     ( ReadData          ),
+      .ReadEnable   ( ReadEnable  ),
+      .ReadAddr     ( ReadAddr    ),
+      .ReadData     ( ReadData    ),
 
-      .WriteEnable  ( WriteEnable_muxed ),
-      .WriteAddr    ( WriteAddr_muxed   ),
-      .WriteData    ( WriteData_muxed   )
+      .WriteEnable  ( WriteEnable ),
+      .WriteAddr    ( WriteAddr   ),
+      .WriteData    ( WriteData   )
    );
 
 endmodule // register_file_1r_1w_test_wrap
