85bb198cef7f91f6322ab56fb544395ce2bbd141
Bjoern Forsberg <bjoernf@iis.ee.ethz.ch>
2020-05-22 10:19:55 +0200

    toolchain/llvm-project: Updated reference to merged subrepo branch (frontend-as-assign -> hero-next). Added compiler smoke test.

---
+++ .gitlab-ci.d/compiler-smoketest.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+#
+# HEROv3 Compiler Smoke Test
+# bjoernf@iis.ee.ethz.ch
+# May 2020.
+#
+# This script tries to compile the benchmarks in the openmp-examples
+# subdirectory using a lot of different flags, and all of their combinations. It
+# is meant to quickly find cases where the compiler crashes, to avoid having to
+# wait for the CI tests to complete. These tests are also currently (21 May '20)
+# broader in scope than the CI, although they do not test for correctness, only
+# that the compiler doesn't crash.
+#
+# Usage:
+#   1. Set HERO_INSTALL environment variable to your HERO install path.
+#   2. cd to the root of the repository.
+#   3. Run script without arguments.
+# This script does not build the infrastructure, so that must be done before, in
+# accordance with the README.
+#
+# Below are the CFLAGS and MAKE flags that we should try all combinations of.
+# ONLY: Selects the benchmarks to be compiled heterogeneously, or only for PULP.
+# If only=pulp is given, then also the main function etc. is compiled with the
+# HERO RISCV compiler, giving a lot larger code coverage.
+only_makeflags=( "only=pulp" "" )
+# DEFAS: Compiles the benchmarks with the "default" default address space (host
+# for heterogeneous compilation, pulp for pulp-only compilation), or explicitly
+# configures it to be host or pulp.
+defas_makeflags=( "" "default-as=host" "default-as=pulp" )
+# UNROLL: I used these flags when testing the hardware loops merge request, to
+# ensure that there were more loops to transform, by avoiding unrolling. I
+# therefore included this as well. Don't know if it is relevant outside that
+# scope.
+unroll_cflags=( "" "-mllvm -unroll-count=1 -mllvm -unroll-full-max-count=1" )
+# The PolyBench-ACC benchmarks have a non-DMA and a DMA-based implementation
+# within them, which can be selected by defining the DPOLYBENCH_DMA preprocessor
+# macro. This vector will compile each benchmark (and combination of other
+# vector values) once with DMA, and once without.
+polydma_cflags=( "" "-DPOLYBENCH_DMA")
+
+source env/exilzcu102.sh
+
+# Try the relevant combinations for the classic openmp-examples.
+makeflags=""
+cflags=""
+## build examples
+for defas in "${defas_makeflags[@]}"; do
+  makeflags="$defas";
+  for unroll in "${unroll_cflags[@]}"; do
+    cflags="$unroll"
+    for d in helloworld mm-large mm-small sobel-filter; do
+      if [ $(basename $d) = "common" ]; then
+        continue
+      fi
+      echo "TEST: $d    $makeflags    $cflags"
+      # FIXME: If this is ever included in the CI, I think we have to remove the
+      # " || exit", because I think that will cause the overall statement to
+      # have return code 0 and thus not properly failing the test.
+      make V=1 -C openmp-examples/$d clean all $makeflags cflags="$cflags" || exit
+    done
+  done
+done
+echo "SUCCESS OPENMP-EXAMPLES!"
+
+# Try the relevant combinations for the polybench benchmarks.
+for only in "${only_makeflags[@]}"; do
+  for defas in "${defas_makeflags[@]}"; do
+    makeflags="$only $defas";
+    for unroll in "${unroll_cflags[@]}"; do
+      for polydma in "${polydma_cflags[@]}"; do
+        cflags="-v $unroll $polydma"
+        for d in openmp-examples/polybench-acc/*/; do
+          if [ $(basename $d) = "common" ]; then
+            continue
+          fi
+          echo "TEST: $d    $makeflags    $cflags"
+          # FIXME: If this is ever included in the CI, I think we have to remove
+          # the " || exit", because I think that will cause the overall
+          # statement to have return code 0 and thus not properly failing the
+          # test.
+          make V=1 -C $d clean all $makeflags cflags="$cflags" || exit
+        done
+      done
+    done
+  done
+done
+echo "SUCCESS POLYBENCH!"
+echo "SUCCESS ALL!"
