b66fb1fc9e5f092ff237f6484de3849976932d73
Andreas Kurth <akurth@iis.ee.ethz.ch>
2020-06-23 09:54:11 +0200

    CI: Simulate all three I$ implementations

--- .gitlab-ci.d/test-sims.sh
+++ .gitlab-ci.d/test-sims.sh
@@ -2,18 +2,25 @@
 
 set -e
 
+def_icache_latches=''
+def_icache_ffs='-define RF_1R1W_FF -define RF_2R2W_FF'
+def_icache_ffs_l15_data_sram="$def_icache_ffs -define USE_DATA_SRAM"
+
 cd hardware
-for sim in vsim vcs; do
-  if [ "$sim" = "vcs" ]; then
-    make vcs/compile.sh
-  fi
-  # Compile testbench
-  cd $sim
-  ./compile.sh
-  for d in "$@"; do
-    make -C ../../example-apps/$d clean all
-    ../test/gen_slm_files.sh $d
-    ./start_sim.sh
+for defines in "$def_icache_latches" "$def_icache_ffs" "$def_icache_ffs_l15_data_sram"; do
+  export DEFINES="$defines"
+  for sim in vsim vcs; do
+    if [ "$sim" = "vcs" ]; then
+      make vcs/compile.sh
+    fi
+    # Compile testbench
+    cd $sim
+    ./compile.sh
+    for d in "$@"; do
+      make -C ../../example-apps/$d clean all
+      ../test/gen_slm_files.sh $d
+      ./start_sim.sh
+    done
+    cd -
   done
-  cd -
 done
