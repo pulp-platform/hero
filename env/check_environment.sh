51ad5d6c212773aaa343b9744338c3a64756df99
Andreas Kurth <akurth@iis.ee.ethz.ch>
2020-11-23 16:30:02 +0100

    env/check_environment: Add `CFLAGS`, `CXXFLAGS`, `LDFLAGS`, and `LDLIBS` to forbidden environment variables

--- env/check_environment.sh
+++ env/check_environment.sh
@@ -39,8 +39,12 @@ should_be_empty_or_undefined() {
 
 must_be_nonempty HERO_INSTALL
 should_be_empty_or_undefined CC
+should_be_empty_or_undefined CFLAGS
 should_be_empty_or_undefined CPLUS_INCLUDE_PATH
 should_be_empty_or_undefined CXX
+should_be_empty_or_undefined CXXFLAGS
 should_be_empty_or_undefined C_INCLUDE_PATH
+should_be_empty_or_undefined LDFLAGS
+should_be_empty_or_undefined LDLIBS
 should_be_empty_or_undefined LD_LIBRARY_PATH
 should_be_empty_or_undefined LIBRARY_PATH
