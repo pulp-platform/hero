3bf813e82fe8c3199a52ba854691dd48e5fd5cb6
Andreas Kurth <akurth@iis.ee.ethz.ch>
2020-12-02 16:47:20 +0100

    openmp-examples/default.mk: Remove `*.elf`s in `clean`

--- openmp-examples/common/default.mk
+++ openmp-examples/common/default.mk
@@ -137,7 +137,7 @@ $(DEPFILES):
 include $(wildcard $(DEPFILES))
 
 clean::
-	-rm -vf __hmpp* $(EXE) *~ *.bc *.dis *.i *.lh *.lk *.ll *.o *.s *.slm
+	-rm -vf __hmpp* $(EXE) *~ *.bc *.dis *.elf *.i *.lh *.lk *.ll *.o *.s *.slm
 	-rm -rvf $(DEPDIR)
 
 init-target-host:
