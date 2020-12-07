5c60ceb7ffbbebc87f0e6eeb81f4f39b560a86c2
Alessandro Capotondi <alessandro.capotondi@unimore.it>
2020-12-07 12:03:52 +0100

    openmp-examples/helloworld: Fix to actually be "HelloWorld" and clean up

--- openmp-examples/helloworld/helloworld.c
+++ openmp-examples/helloworld/helloworld.c
@@ -18,30 +18,21 @@
 
 #include <omp.h>
 #include <stdio.h>
-#include <stdint.h>
-#include <time.h>
 #include <hero-target.h>
-
-struct timespec start, stop;
-double start_ns, stop_ns, exe_time;
 
 #pragma omp declare target
 void helloworld (void)
 {
-	#pragma omp parallel
+#pragma omp parallel
 	printf("Hello World, I am thread %d of %d\n", omp_get_thread_num(), omp_get_num_threads());
 }
 #pragma omp end declare target
 
 int main(int argc, char *argv[])
 {
-  omp_set_default_device(BIGPULP_MEMCPY);
 
-  uint64_t k = (uint64_t)&argc;
-  #pragma omp target map(tofrom: k)
-  {
-    printf("POINTER: 0x%llx\n", (uint64_t)k);
-  }
-  printf("POINTER: 0x%llx\n", (uint64_t)k);
-  return 0;
+#pragma omp target
+	helloworld();
+
+	return 0;
 }
