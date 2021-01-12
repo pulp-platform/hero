0bae9f83df8f3bfee0cb80c99fff0b13b741b3cb
Maxim Mattheeuws <mmaxim@iis.ee.ethz.ch>
2021-01-12 10:31:14 +0100

    PREM: Revert to non-PREM version

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+ROOT=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)
+
+set -e
+
+if [ -z "$HERO_INSTALL" ]; then
+  echo "Fatal: 'HERO_INSTALL' environment variable is not set!"
+  exit 1
+fi
+
+if [ -z "$CMUX_ROOT" ]; then
+  echo "Fatal: 'CMUX_ROOT' environment variable is not set!"
+  exit 1
+fi
+
+make -C "$ROOT" tc-pulp
+make -C "$ROOT" tc-har-olinux
+make -C "$ROOT" sdk-pulp
+make -C "$ROOT" sdk-har
+make -C "$ROOT" tc-llvm
+make -C "$ROOT" br-har-exilzcu102
