586b822ae4063c54f97a737a5b0f910cd5639665
Andreas Kurth <akurth@iis.ee.ethz.ch>
2021-01-22 14:52:06 +0100

    board: Remove configurations unused in this project

--- board/diggenesys2/post_image.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Creating binary image"
-TARGET_TUPLE=$(grep BR2_TOOLCHAIN_EXTERNAL_CUSTOM_PREFIX ${BR2_CONFIG} | sed 's/.*=//' | sed 's/$(ARCH)/riscv64/' | tr -d '"')
-$TARGET_TUPLE-objcopy -S -O binary --change-addresses -0x80000000 $1/bbl $1/bbl.bin
