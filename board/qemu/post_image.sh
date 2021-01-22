0ca0d19d6f99f2eced960d02d9d84faa1e98184d
Andreas Kurth <akurth@iis.ee.ethz.ch>
2021-01-22 14:52:06 +0100

    board: Remove configurations unused in this project

--- board/qemu/post_image.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# FIXME: ensures bbl is always up to date, as bootloader does not auto update after changes
-echo "Recreating bbl with payload"
-make -C ${BASE_DIR} riscv-pk-rebuild
