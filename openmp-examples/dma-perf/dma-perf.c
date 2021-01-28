469c045f7b0ff0b81d8f90549fb8854bef8d1417
Andreas Kurth <akurth@iis.ee.ethz.ch>
2021-01-28 16:06:56 +0100

    openmp-examples/dma-perf: Clarify units

--- openmp-examples/dma-perf/dma-perf.c
+++ openmp-examples/dma-perf/dma-perf.c
@@ -26,45 +26,44 @@
 
 #define BUF_L3 (void*) 0x80000000
 
-void perform_benchmark(const unsigned buf_size_kb) {
+void perform_benchmark(const unsigned buf_size_kib) {
     unsigned cyc_31, cyc_13;
-    const unsigned buf_size = buf_size_kb * 1024;
+    const unsigned buf_size_bytes = buf_size_kib * 1024;
 
-    #pragma omp target device(BIGPULP_MEMCPY) map(to: buf_size) map(from: cyc_13, cyc_31)
+    #pragma omp target device(BIGPULP_MEMCPY) map(to: buf_size_bytes) map(from: cyc_13, cyc_31)
     {
-        __device uint32_t* const buf_l1 = (__device uint32_t*) hero_l1malloc(buf_size);
+        __device uint32_t* const buf_l1 = (__device uint32_t*) hero_l1malloc(buf_size_bytes);
         if (buf_l1 == NULL){
           printf("ERROR: hero_l1malloc() failed\n");
         }
 
         // L1 to L3 (RAM) with DMA
         hero_reset_clk_counter();
-        hero_memcpy_host2dev(buf_l1, (__host void*) BUF_L3, buf_size);
+        hero_memcpy_host2dev(buf_l1, (__host void*) BUF_L3, buf_size_bytes);
         cyc_31 = hero_get_clk_counter();
 
         // L3 (RAM) to L1 with DMA
         hero_reset_clk_counter();
-        hero_memcpy_dev2host((__host void*) BUF_L3, buf_l1, buf_size);
+        hero_memcpy_dev2host((__host void*) BUF_L3, buf_l1, buf_size_bytes);
         cyc_13 = hero_get_clk_counter();
-
 
         hero_l1free(buf_l1);
     }
 
-    const double perf_31 = ((double) buf_size) / cyc_31;
-    const double perf_13 = ((double) buf_size) / cyc_13;
-    printf("For buffer size %dkB:\n", buf_size_kb);
-    printf("L3 -> L1 DMA:  %f bytes / cycle\n", perf_31);
-    printf("L1 -> L3 DMA:  %f bytes / cycle\n", perf_13);
+    const double perf_31 = ((double) buf_size_bytes) / cyc_31;
+    const double perf_13 = ((double) buf_size_bytes) / cyc_13;
+    printf("For transfer size %d KiB:\n", buf_size_kib);
+    printf("L3 -> L1 DMA:  %.3f bytes / cycle\n", perf_31);
+    printf("L1 -> L3 DMA:  %.3f bytes / cycle\n", perf_13);
 }
 
 int main(int argc, char *argv[]) {
   omp_set_default_device(BIGPULP_SVM);
 
-  //unsigned buffer_sizes[] = {1, 2, 4, 8, 16, 32, 64};
-  const unsigned buffer_sizes[] = {110};
-  for(unsigned i = 0; i < sizeof(buffer_sizes) / sizeof(unsigned); i++) {
-    perform_benchmark(buffer_sizes[i]);
+  //unsigned buf_size_kib[] = {1, 2, 4, 8, 16, 32, 64};
+  const unsigned buf_size_kib[] = {110};
+  for(unsigned i = 0; i < sizeof(buf_size_kib) / sizeof(unsigned); i++) {
+    perform_benchmark(buf_size_kib[i]);
   }
 
   return 0;
