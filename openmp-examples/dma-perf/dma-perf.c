6e7a4a6f534280156d8914d653b7d1758c8dad20
Andreas Kurth <akurth@iis.ee.ethz.ch>
2021-01-29 19:50:54 +0100

    openmp-examples/dma-perf: Copy to/from allocated memory and compare result

--- openmp-examples/dma-perf/dma-perf.c
+++ openmp-examples/dma-perf/dma-perf.c
@@ -22,13 +22,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define BUF_L3 (void*) 0x80000000
+uint16_t lfsr3(uint16_t prev) {
+  prev ^= prev >> 7;
+  prev ^= prev << 9;
+  prev ^= prev >> 13;
+  return prev;
+}
 
 void perform_benchmark(const unsigned buf_size_kib) {
   unsigned cyc_31, cyc_13;
   const unsigned buf_size_bytes = buf_size_kib * 1024;
 
-#pragma omp target device(BIGPULP_MEMCPY) map(to : buf_size_bytes) map(from : cyc_13, cyc_31)
+  // Allocate source and destination buffers on heap of Host.
+  uint8_t* const src_buf = (uint8_t*)malloc(buf_size_bytes);
+  uint8_t* const dst_buf = (uint8_t*)malloc(buf_size_bytes);
+  if (src_buf == NULL || dst_buf == NULL) {
+    printf("Error: malloc() on host failed!\n");
+    exit(-1);
+  }
+
+  // Initialize source buffer with pseudo-random data.
+  const uint16_t lfsr_init = 0xB1AAu;
+  uint16_t lfsr = lfsr_init;
+  for (unsigned i = 0; i < buf_size_bytes / 2; i++) {
+    ((uint16_t*)src_buf)[i] = lfsr;
+    lfsr = lfsr3(lfsr);
+  }
+
+#pragma omp target device(BIGPULP_MEMCPY) map(to : buf_size_bytes, src_buf[0 : buf_size_bytes]) \
+    map(from : cyc_13, cyc_31, dst_buf[0 : buf_size_bytes])
   {
     __device uint32_t* const buf_l1 = (__device uint32_t*)hero_l1malloc(buf_size_bytes);
     if (buf_l1 == NULL) {
@@ -38,22 +60,45 @@ void perform_benchmark(const unsigned buf_size_kib) {
 
     // L1 to L3 (RAM) with DMA
     hero_reset_clk_counter();
-    hero_memcpy_host2dev(buf_l1, (__host void*)BUF_L3, buf_size_bytes);
+    hero_memcpy_host2dev(buf_l1, (__host void*)src_buf, buf_size_bytes);
     cyc_31 = hero_get_clk_counter();
 
     // L3 (RAM) to L1 with DMA
     hero_reset_clk_counter();
-    hero_memcpy_dev2host((__host void*)BUF_L3, buf_l1, buf_size_bytes);
+    hero_memcpy_dev2host((__host void*)dst_buf, buf_l1, buf_size_bytes);
     cyc_13 = hero_get_clk_counter();
 
     hero_l1free(buf_l1);
   }
 
-  const double perf_31 = ((double)buf_size_bytes) / cyc_31;
-  const double perf_13 = ((double)buf_size_bytes) / cyc_13;
+  // Free source buffer.  We do not use it to compare with the destination buffer because they could
+  // be both corrupted in the same way.
+  free(src_buf);
+
+  // Compare destination buffer to pseudo-random values.
+  unsigned mismatches = 0;
+  lfsr = lfsr_init;
+  for (unsigned i = 0; i < buf_size_bytes / 2; i++) {
+    if (((uint16_t*)dst_buf)[i] != lfsr) {
+      mismatches++;
+      printf("[%04d] 0x%04x != 0x%04x\n", i, ((uint16_t*)dst_buf)[i], lfsr);
+    }
+    lfsr = lfsr3(lfsr);
+  }
+
+  // Free destination buffer.
+  free(dst_buf);
+
+  // Print result.
   printf("For transfer size %d KiB:\n", buf_size_kib);
-  printf("L3 -> L1 DMA: %.3f bytes/cycle\n", perf_31);
-  printf("L1 -> L3 DMA: %.3f bytes/cycle\n", perf_13);
+  if (mismatches == 0) {
+    const double perf_31 = ((double)buf_size_bytes) / cyc_31;
+    const double perf_13 = ((double)buf_size_bytes) / cyc_13;
+    printf("L3 -> L1 DMA: %.3f bytes/cycle\n", perf_31);
+    printf("L1 -> L3 DMA: %.3f bytes/cycle\n", perf_13);
+  } else {
+    printf("%d mismatches!\n", mismatches);
+  }
 }
 
 int main(int argc, char* argv[]) {
