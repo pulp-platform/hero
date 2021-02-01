3107ce6cc40502ff93bcd74c5bf7e160d898216f
Bjoern Forsberg <bjoernf@iis.ee.ethz.ch>
2021-02-01 13:26:31 +0100

    package/prem-cmux: Install CMUX lib/includes used by libomptarget. Improve cross-compile

--- package/prem-cmux/prem-cmux.mk
+++ package/prem-cmux/prem-cmux.mk
@@ -13,21 +13,21 @@ PREM_CMUX_INSTALL_TARGET = YES
 PREM_CMUX_MAKE_ENV = $(TARGET_MAKE_ENV) CROSS_COMPILE=$(TARGET_CROSS)
 
 define PREM_CMUX_BUILD_CMDS
-	echo $(PWD)
 	$(PREM_CMUX_MAKE_ENV) $(MAKE) -C $(PREM_CMUX_SITE) cmux
 endef
 
 define PREM_CMUX_INSTALL_STAGING_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/bin/cmux $(STAGING_DIR)/usr/bin/cmux
-	$(INSTALL) -D -m 0644 $(@D)/lib/libcmux-vote.a $(STAGING_DIR)/usr/lib/libcmux-vote.a
-	$(INSTALL) -D -m 0644 $(@D)/lib/libpremnotify-cpu.so $(STAGING_DIR)/usr/lib/libpremnotify.so
+	$(INSTALL) -D -m 0755 $(PREM_CMUX_SITE)/bin/cmux $(STAGING_DIR)/usr/bin/cmux
+	$(INSTALL) -D -m 0644 $(PREM_CMUX_SITE)/lib/libcmux-vote.a $(STAGING_DIR)/usr/lib/libcmux-vote.a
+	$(INSTALL) -D -m 0644 $(PREM_CMUX_SITE)/lib/libpremnotify-cpu.so $(STAGING_DIR)/usr/lib/libpremnotify.so
+  # TODO We should move all of these headers to the prem subdirectory to keep them organized
 	mkdir -p $(STAGING_DIR)/usr/include/cmux
-	$(INSTALL) -D -m 0644 $(@D)/inc/cmuxif.h $(STAGING_DIR)/usr/include/prem/cmuxif.h
-	$(INSTALL) -D -m 0644 $(@D)/inc/vote.h $(STAGING_DIR)/usr/include/prem/vote.h
-endef
-
-define LIBHERO_TARGET_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/bin/cmux $(TARGET_DIR)/usr/bin/cmux
+	$(INSTALL) -D -m 0644 $(PREM_CMUX_SITE)/inc/cmuxif.h $(STAGING_DIR)/usr/include/prem/cmuxif.h
+	$(INSTALL) -D -m 0644 $(PREM_CMUX_SITE)/inc/vote.h $(STAGING_DIR)/usr/include/prem/vote.h
+	$(INSTALL) -D -m 0644 $(PREM_CMUX_SITE)/inc/vote.h $(STAGING_DIR)/usr/include/vote.h
+	$(INSTALL) -D -m 0644 $(PREM_CMUX_SITE)/inc/libpremnotify.h $(STAGING_DIR)/usr/include/libpremnotify.h
+	$(INSTALL) -D -m 0644 $(PREM_CMUX_SITE)/inc/channel.h $(STAGING_DIR)/usr/include/channel.h
+	$(INSTALL) -D -m 0644 $(PREM_CMUX_SITE)/inc/logprint.h $(STAGING_DIR)/usr/include/logprint.h
 endef
 
 $(eval $(generic-package))
