eaa5ba280dc5224b914038529bab7fdf12aae702
Andreas Kurth <akurth@iis.ee.ethz.ch>
2021-03-05 16:40:42 +0100

    openmp-examples/darknet/static_layer_generator: Simplify with triple quotes and string formatting

--- openmp-examples/darknet/static_layer_generator.py
+++ openmp-examples/darknet/static_layer_generator.py
@@ -10,39 +10,47 @@ N = ["173056", "43264", "10816","2704","676", "169", "169", "169", "169", "169",
 K = ["27", "144", "288", "576", "1152", "2304", "4608", "1024", "2304", "512", "256", "3456", "256"]
 
 with open('./gemm_layers.c', 'w') as f:
-  f.write('// Copyright (c) 2020 ETH Zurich and University of Bologna\n')
-  f.write('// Licensed under the Apache License, Version 2.0; see LICENSE.Apache-2.0 for details.\n')
-  f.write('// SPDX-License-Identifier: Apache-2.0\n\n')
+  f.write("""\
+// Copyright (c) 2020 ETH Zurich and University of Bologna
+// Licensed under the Apache License, Version 2.0; see LICENSE.Apache-2.0 for details.
+// SPDX-License-Identifier: Apache-2.0
 
-  f.write('#include <hero-target.h>\n\n')
-  f.write("extern int LAYER_COUNTER;\n\n")
+#include <hero-target.h>
+
+extern int LAYER_COUNTER;
+
+""")
 
   for i in range(len(layer)):
 
-      f.write("void gemm_" + layer[i] + "(float ALPHA, float *A, float *B, float *C){\n\
-  const int M = " + M[i] + ";\n\
-  const int N = " + N[i] + ";\n\
-  const int K = " + K[i] + ";\n\
-  __host float (*matA)[K] = (__host float(*)[K]) A;\n\
-  __host float (*matB)[N] = (__host float(*)[N]) B;\n\
-  __host float (*matC)[N] = (__host float(*)[N]) C;\n\
-  #pragma omp target data device(BIGPULP_MEMCPY) map(to: matB[0:" + K[i] + "][0:" + N[i] + "])\n\
-  {\n\
-    #pragma omp target data device(BIGPULP_MEMCPY) map(to: matA[0:" + M[i] + "][0:" + K[i] + "])\n\
-    {\n\
-      #pragma omp target device(BIGPULP_MEMCPY) map(tofrom: matC[0:" + M[i] + "][0:" + N[i] + "])\n\
-      {\n\
-        #pragma omp parallel for num_threads(8) schedule(static, 1)\n\
-        for(int m = 0; m < M; ++m){\n\
-          for(int k = 0; k < K; ++k){\n\
-            float temp = ALPHA*matA[m][k];\n\
-            for(int n = 0; n < N; ++n){\n\
-              matC[m][n] +=temp*matB[k][n];\n\
-            }\n\
-          }\n\
-        }\n\
-      }\n\
-    }\n\
-  }\n\
-  LAYER_COUNTER=" + layer[(i+1)%len(layer)] + ";\n\
-}\n\n\n")
+    f.write("""\
+void gemm_{layer}(float ALPHA, float *A, float *B, float *C){{
+  const int M = {M};
+  const int N = {N};
+  const int K = {K};
+  __host float (*matA)[K] = (__host float(*)[K]) A;
+  __host float (*matB)[N] = (__host float(*)[N]) B;
+  __host float (*matC)[N] = (__host float(*)[N]) C;
+  #pragma omp target data device(BIGPULP_MEMCPY) map(to: matB[0:{K}][0:{N}])
+  {{
+    #pragma omp target data device(BIGPULP_MEMCPY) map(to: matA[0:{M}][0:{K}])
+    {{
+      #pragma omp target device(BIGPULP_MEMCPY) map(tofrom: matC[0:{M}][0:{N}])
+      {{
+        #pragma omp parallel for num_threads(8) schedule(static, 1)
+        for(int m = 0; m < M; ++m){{
+          for(int k = 0; k < K; ++k){{
+            float temp = ALPHA*matA[m][k];
+            for(int n = 0; n < N; ++n){{
+              matC[m][n] +=temp*matB[k][n];
+            }}
+          }}
+        }}
+      }}
+    }}
+  }}
+  LAYER_COUNTER={next_layer};
+}}
+
+
+""".format(layer=layer[i], M=M[i], N=N[i], K=K[i], next_layer=layer[(i+1)%len(layer)]))
