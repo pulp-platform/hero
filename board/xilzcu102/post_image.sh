d1670160fe1ad9d5c979d161bedae7b6ffe84276
Andreas Kurth <akurth@iis.ee.ethz.ch>
2022-04-13 16:57:42 +0200

    board/xilzcu102: Remove Buildroot's host binaries from path used by PetaLinux

--- board/xilzcu102/post_image.sh
+++ board/xilzcu102/post_image.sh
@@ -3,9 +3,17 @@
 # set system from hero device tree
 #ln -sf hero.dtb ${BINARIES_DIR}/system.dtb
 
+# Remove Buildroot's host binaries from the path used by PetaLinux, because those binaries can cause
+# problems with PetaLinux.
+readonly PATH_FOR_PETALINUX=$(echo $PATH \
+    | tr ':' '\n' \
+    | grep -E -v "^$BR2_EXTERNAL_HERO_PATH/output/br-har-exilzcu102/host/s?bin$" \
+    | perl -pe 'chomp if eof' \
+    | tr '\n' ':')
+
 # build the petalinux zcu102 config
 cd ${BR2_EXTERNAL_HERO_PATH}/petalinux/
-./zcu102.sh
+env PATH=$PATH_FOR_PETALINUX ./zcu102.sh
 cp ${BR2_EXTERNAL_HERO_PATH}/petalinux/zcu102/images/linux/{bl31.bin,bl31.elf,image.ub,Image,pmufw.elf,regs.init,system.dtb,System.map.linux,u-boot.bin,zynqmp_fsbl.elf} $1
 cp ${BR2_EXTERNAL_HERO_PATH}/petalinux/zcu102/images/linux/BOOT.BIN $1/boot.bin
 cd -
