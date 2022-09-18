169da09d37c402cfdb0a009500f1f343425ea440
Alessandro Capotondi <alessandro.capotondi@unimore.it>
2022-09-18 16:56:10 +0200

    Added setup.sh

---
+++ setup.sh
@@ -0,0 +1,12 @@
+# PULP SDK Env
+export PULP_ARTIFACTORY_DISTRIB=Ubuntu_16
+export PULP_GITHUB_HTTPS=1
+
+# HERO SDK Env
+export HERO_INSTALL=$PWD/INSTALL
+
+export VIVADO="vivado"
+export NO_IIS=1
+
+# Source sim environment
+source $PWD/env/esim-exilzcu102.sh
