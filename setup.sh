8c1d530e1d9aa614aaea5ff345458f0c37265b24
Alessandro Capotondi <alessandro.capotondi@unimore.it>
2022-09-19 11:34:42 +0200

    Fixed setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 # PULP SDK Env
-export PULP_ARTIFACTORY_DISTRIB=Ubuntu_16
 export PULP_GITHUB_HTTPS=1
 
 # HERO SDK Env
 export HERO_INSTALL=$PWD/INSTALL
 
+# Remove these two configurations if you use ETHZ IIS Machines
 export VIVADO="vivado"
 export NO_IIS=1
 
